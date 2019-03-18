64330abda0fc73c88de6fb8865cb08469f3f98ce
Donald Cross <donaldcross@Donalds-MBP.home>
2019-03-18 05:29:25 -0400

    echobot run

--- bot.js
+++ bot.js
@@ -1,21 +1,303 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
+const { ActivityTypes, MessageFactory, TurnContext   } = require('botbuilder');
+const { LuisRecognizer } = require('botbuilder-ai');
+const axios = require('axios');
+const { DialogSet, WaterfallDialog, TextPrompt, NumberPrompt, ChoicePrompt, DialogTurnStatus} = require('botbuilder-dialogs')
+const DIALOG_STATE_PROPERTY = 'dialogState';
+const USER_PROFILE_PROPERTY = 'user';
+const MAIN_DIALOG = 'Main_Dialog';
+const CHITCHAT_DIALOG = 'Chitchat_Dialog';
+const TEXT = 'Text';
+const timeout = ms => new Promise(resolve => setTimeout(resolve, ms));
 
-const { ActivityTypes } = require('botbuilder');
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').load();
+}
+const path = require('path');
+const storage = require('azure-storage');
+const blobService = storage.createBlobService();
 
-class MyBot {
-    /**
-     *
-     * @param {TurnContext} on turn context object.
-     */
+
+
+class BasicBot {
+    constructor(conversationState, userState, luisApplication, luisPredictionOptions){
+        this.userState = userState;
+        this.conversationState = conversationState;
+        this.conversationReference = this.conversationState.createProperty('CONVERSATION_REFERENCE');
+        // this.luisRecognizer = new LuisRecognizer(luisApplication, luisPredictionOptions, true);
+        this.dialogState = this.conversationState.createProperty(DIALOG_STATE_PROPERTY);
+        this.userProfile = this.userState.createProperty(USER_PROFILE_PROPERTY);
+        this.dialogs = new DialogSet(this.dialogState);
+        this.dialogs.add(new TextPrompt(TEXT));
+        this.dialogs.add(new WaterfallDialog(MAIN_DIALOG, [
+            this.call_fun_1.bind(this),
+            this.call_fun_2.bind(this),
+            this.call_fun_3.bind(this)
+        ]));
+        this.dialogs.add(new WaterfallDialog(CHITCHAT_DIALOG, [
+            this.call_fun_1.bind(this),
+            this.call_fun_2.bind(this),
+            this.call_fun_3.bind(this)
+        ]));
+    }
     async onTurn(turnContext) {
-        // See https://aka.ms/about-bot-activity-message to learn more about the message and other activity types.
+        
+        // const lresults = await this.luisRecognizer.recognize(turnContext);
+        // const topIntent = lresults.luisResult.topScoringIntent;
+        
         if (turnContext.activity.type === ActivityTypes.Message) {
-            await turnContext.sendActivity(`You said '${ turnContext.activity.text }'`);
+            if (turnContext.activity.attachments && turnContext.activity.attachments.length > 0) {
+                // The user sent an attachment and the bot should handle the incoming attachment.
+                await this.handleIncomingAttachment(turnContext);
+            } 
+
+
+            const text = turnContext.activity.text;
+            console.log(turnContext);
+            // if (turnContext.activity.channelId === 'facebook') {
+            //     processFacebookPayload(turnContext.activity.channelData);
+            
+                // console.log(topIntent.intent)
+                // console.log(topIntent)
+                // moved create context inside a switch case to test posible conflicts
+            
+                // if(results.status===DialogTurnStatus.waiting){  
+                //     // skip while waiting intent
+                // } 
+                // else {
+                        
+                    
+                        // let text = topIntent.intent;
+                        switch (text) {
+                            case 'getBack':
+                                await turnContext.sendActivity(`Can do! I'll be kind and rewind our chat`);
+                                return await dc.replaceDialog(MAIN_DIALOG);
+                        
+                            case 'getBotInfo':
+
+                                await turnContext.sendActivity(`I collect uplifting moments shared by people all over the world. Every week, I put the together in a video like this`);
+                                await timeout(1500);
+                                await turnContext.sendActivity(`Want in? I'll tell you how`);
+                                const dc = await this.dialogs.createContext(turnContext);
+                                const results = await dc.continueDialog();
+                                console.log(results.status)
+                                break;
+                            case 'getChitChat':
+                                await this.sendChitChatMessages(turnContext);
+                                break;
+                            case 'getContinue':
+                                await turnContext.sendActivity('Great, I will keep going');
+                                // await dc.beginDialog(MAIN_DIALOG);
+                                break;
+                            case 'getFinish_Conversation':
+                                await turnContext.sendActivity(`Okey, we'll call it a day. Till next time!`);
+                                await dc.endDialog(MAIN_DIALOG);
+                                break;
+                            case 'getGreeting_daily':
+                                await this.sendGreeting_dailyMessages(turnContext);
+                                break;
+                            case 'getGreeting_Inchat':
+                                await this.sendGreeting_InchatMessages(turnContext);
+                                break;
+                            case 'getNotNow':
+                                await this.sendNotNowMessages(turnContext);
+                                break;
+                            case 'getPrompt':
+                                await this.sendPromptMessages(turnContext);
+                                break;
+                            case 'getStarted':
+                                await this.sendStartMessages(turnContext);
+                                break;
+                            case 'getVideo_LastWeek':
+                                await this.sendVideo_LastWeekMessages(turnContext);
+                                break;
+                            case 'getVideo_ThisWeek':
+                                await this.sendVideo_ThisWeekMessages(turnContext);
+                                break;
+                            default :
+                                
+                                    // await turnContext.sendActivity(`Come again? I didn't understand that`);
+                                    var reply = MessageFactory.suggestedActions(['getBack', 'getBotInfo', 'getChitChat', 'getContinue'],`Come again?, I didn't understand that.`);
+                                    await turnContext.sendActivity(reply);
+                                    // await this.sendDefaultMessages(turnContext);
+                                
+                        }
+                    
+                // }
+                    // if (!turnContext.responded) {
+                    //     await turnContext.sendActivity('...');
+                    //     await dc.beginDialog(MAIN_DIALOG);
+                    // } 
+            // }  // end of facebook channel data 
+            // const utterance = (turnContext.activity.text || '').trim().toLowerCase();
+
+            
+        } else if(turnContext.activity.type === ActivityTypes.ConversationUpdate) {
+            await this.storeConversationReference(turnContext);
+            if (turnContext.activity.membersAdded && turnContext.activity.membersAdded.length > 0) {
+                await this.sendWelcomeMessage(turnContext);
+            }
         } else {
-            await turnContext.sendActivity(`[${ turnContext.activity.type } event detected]`);
+            await turnContext.sendActivity(`[${ turnContext.activity.type } event detected.]`);
+            if (turnContext.activity.channelId === 'facebook') {
+                // Analyze Facebook payload from channel data to trigger custom events.
+                processFacebookPayload(turnContext.activity.channelData);
+            }
+        }
+        // console.log(turnContext.activity.recipient.id);
+        await this.userState.saveChanges(turnContext);
+        await this.conversationState.saveChanges(turnContext);
+    }
+
+    async localDownloadAttachment(attachment){
+        let url = attachment.contentUrl;
+            let dir_local = __dirname + '/files';
+            const localFileName = path.join(dir_local, attachment.name);
+
+            try {
+                const response = await axios.get(url, { responseType: 'arraybuffer' });
+                if (response.headers['content-type'] === 'application/json') {
+                    response.data = JSON.parse(response.data, (key, value)=> {
+                        return value && value.type === 'Buffer' ?
+                        Buffer.from(value.data): 
+                        value;
+                    });
+                } 
+                fs.writeFile(localFileName, response.data, (fsError)=> {
+                    if(fsError){
+                        throw fsError;
+                    }
+                });
+            } catch (error){
+                console.error(error);
+                return undefined;
+            }
+            return {
+                localFileName: attachment.name,
+                localPath: localFilename
+            };
+    }
+
+    async handleIncomingAttachment(turnContext) {
+        const promises = turnContext.activity.attachments.map((attachment)=>{this.downloadAttachmentAndWrite(attachment,turnContext)});
+        await Promise.all(promises);
+    }
+    
+    async downloadAttachmentAndWrite(attachment,turnContext) {
+        const reference = await this.conversationReference.get(turnContext);
+        if (reference) { console.log('here is the reference object:   ' + reference) }
+        
+        if(reference.channelId===emulator){
+            result = localDownloadAttachment(attachment);
+            console.log(result);
+            
+        } else if(reference.channelId===facebook){
+            let obj_filename = attachment.name + reference.user.id + '__' + Date.now() 
+            let obj_url = 'https://amp.businessinsider.com/images/58584681a1a45e46008b6dcd-750-563.png';
+            let containerName = 'ojoy-storage';
+            //set conversation reference in blob metadata
+            var options = { metadata: { "conversation_reference": JSON.stringify(reference) } };
+            //get file buffer stream
+            const response = await axios.get(obj_url, { responseType: 'stream' });
+            //get file content length
+            let size = response.headers['content-length'];
+            
+                return new Promise((resolve, reject) => {
+                    blobService.createBlockBlobFromStream(containerName, obj_filename, response.data, size, options, err => {
+                        if (err) {
+                            reject(err);
+                        } else {
+                            resolve();
+                        }
+                    });
+                });  
         }
     }
+    async storeConversationReference(turnContext) {
+        // pull the reference
+        const reference = TurnContext.getConversationReference(turnContext.activity);
+        // store reference in memory using conversation data property
+        await this.conversationReference.set(turnContext, reference);
+    }
+
+    async sendWelcomeMessage(turnContext) {
+        for (var idx in turnContext.activity.membersAdded) {
+            if (turnContext.activity.membersAdded[idx].id !== turnContext.activity.recipient.id) {
+                await turnContext.sendActivity(`I’m a bot that serves up feel-good moments. Ever wonder how your idea of Love compares to everyone else's?`);
+                await timeout(2500);
+                await turnContext.sendActivity(`Watch this to see what I mean`)  
+                await timeout(1500);
+                await turnContext.sendActivity(`Video`)
+                await timeout(2500);
+                if (turnContext.activity.channelId === 'facebook') {
+                    processFacebookPayload(turnContext.activity.channelData);
+                    var reply = MessageFactory.suggestedActions(['getBotInfo'], '');
+                    await turnContext.sendActivity(reply);       
+                } // end of processFacebookPayload
+            }
+        }
+    }
+
+    async sendChitChatMessages(turnContext) {
+        let url = "https://api.chucknorris.io/jokes/random";
+        let res = await axios.get(url);
+        await turnContext.sendActivity(res.data.value);
+        await this.sendDefaultMessages(turnContext);
+    }
+
+    async sendDefaultMessages(turnContext){
+            // var reply = MessageFactory.suggestedActions(['getBack', 'getBotInfo', 'getChitChat', 'getContinue'],``);
+            // await turnContext.sendActivity(reply);
+    }
+
+    async call_fun_1(step){
+        await step.context.sendActivity('step 1');
+        return await step.prompt(TEXT, '');
+    }
+    async call_fun_2(step){
+        await step.context.sendActivity('step 2');
+        return await step.prompt(TEXT, '');
+        // await step.context.sendActivity(step.result);
+        // var reply = MessageFactory.suggestedActions(['BACK', 'NOT_NOW'], '');
+        // await step.context.sendActivity(reply);
+        // return reply;
+    }
+    async call_fun_3(step){
+        await step.context.sendActivity('step 3');
+        return await step.prompt(TEXT, '');
+        // if(step.result==='BACK'){
+        //     return await step.replaceDialog(MAIN_DIALOG);
+        // } else {
+        //     await step.context.sendActivity(step.result);
+        // }
+    }
+}
+module.exports.BasicBot = BasicBot;
+
+function processFacebookPayload(channelData) {
+    if (!channelData) {
+        return;
+    }
+
+    if (channelData.postback) {
+        onFacebookPostback(channelData.postback);
+    } else if (channelData.optin) {
+        onFacebookOptin(channelData.optin);
+    } else if (channelData.message && channelData.message.quick_reply) {
+        onFacebookQuickReply(channelData.message.quick_reply);
+    } else {
+        // TODO: Handle other events that you're interested in...
+    }
+}
+function onFacebookOptin(optin) {
+    // TODO: Your optin handling logic here...
 }
 
-module.exports.MyBot = MyBot;
+function onFacebookPostback(postback) {
+    // TODO: Your postBack handling logic here...
+}
+
+function onFacebookQuickReply(quickReply) {
+    // TODO: Your QuickReply handling logic here...
+    
+}
+
--- index.js
+++ index.js
@@ -1,81 +1,266 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-const dotenv = require('dotenv');
+// index.js is used to setup and configure your bot
+
+// Import required pckages
 const path = require('path');
 const restify = require('restify');
+const axios = require('axios');
 
-// Import required bot services.
-// See https://aka.ms/bot-services to learn more about the different parts of a bot.
-const { BotFrameworkAdapter } = require('botbuilder');
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').load();
+}
 
+const storage = require('azure-storage');
+const blobService = storage.createBlobService();
+
+// Import required bot services. See https://aka.ms/bot-services to learn more about the different parts of a bot.
+const { BotFrameworkAdapter, MemoryStorage, ConversationState, UserState, ActivityTypes, TurnContext } = require('botbuilder');
 // Import required bot configuration.
 const { BotConfiguration } = require('botframework-config');
 
 // This bot's main dialog.
-const { MyBot } = require('./bot');
+const { BasicBot } = require('./bot');
 
 // Read botFilePath and botFileSecret from .env file
 // Note: Ensure you have a .env file and include botFilePath and botFileSecret.
 const ENV_FILE = path.join(__dirname, '.env');
-dotenv.config({ path: ENV_FILE });
+const env = require('dotenv').config({ path: ENV_FILE });
 
-// bot endpoint name as defined in .bot file
+// Get the .bot file path
 // See https://aka.ms/about-bot-file to learn more about .bot file its use and bot configuration.
-const DEV_ENVIRONMENT = 'development';
-
-// bot name as defined in .bot file
-// See https://aka.ms/about-bot-file to learn more about .bot file its use and bot configuration.
-const BOT_CONFIGURATION = (process.env.NODE_ENV || DEV_ENVIRONMENT);
-
-// Create HTTP server
-const server = restify.createServer();
-server.listen(process.env.port || process.env.PORT || 3978, () => {
-    console.log(`\n${ server.name } listening to ${ server.url }`);
-    console.log(`\nGet Bot Framework Emulator: https://aka.ms/botframework-emulator`);
-    console.log(`\nTo talk to your bot, open echobot.bot file in the Emulator`);
-});
-
-// .bot file path
 const BOT_FILE = path.join(__dirname, (process.env.botFilePath || ''));
-
-// Read bot configuration from .bot file.
 let botConfig;
 try {
+    // Read bot configuration from .bot file.
     botConfig = BotConfiguration.loadSync(BOT_FILE, process.env.botFileSecret);
 } catch (err) {
     console.error(`\nError reading bot file. Please ensure you have valid botFilePath and botFileSecret set for your environment.`);
-    console.error(`\n - The botFileSecret is available under appsettings for your Azure Bot Service bot.`);
+    console.error(`\n - You can find the botFilePath and botFileSecret in the Azure App Service application settings.`);
     console.error(`\n - If you are running this bot locally, consider adding a .env file with botFilePath and botFileSecret.`);
     console.error(`\n - See https://aka.ms/about-bot-file to learn more about .bot file its use and bot configuration.\n\n`);
     process.exit();
 }
 
+// For local development configuration as defined in .bot file
+const DEV_ENVIRONMENT = 'development';
+
+// bot name as defined in .bot file or from runtime
+const BOT_CONFIGURATION = (process.env.NODE_ENV || DEV_ENVIRONMENT);
+
 // Get bot endpoint configuration by service name
 const endpointConfig = botConfig.findServiceByNameOrId(BOT_CONFIGURATION);
 
-// Create adapter.
-// See https://aka.ms/about-bot-adapter to learn more about .bot file its use and bot configuration.
+// Create adapter. 
+// See https://aka.ms/about-bot-adapter to learn more about .bot file its use and bot configuration .
 const adapter = new BotFrameworkAdapter({
     appId: endpointConfig.appId || process.env.microsoftAppID,
-    appPassword: endpointConfig.appPassword || process.env.microsoftAppPassword
+    appPassword: endpointConfig.appPassword || process.env.microsoftAppPassword,
+    channelService: process.env.ChannelService,
+    openIdMetadata: process.env.BotOpenIdMetadata
 });
 
 // Catch-all for errors.
 adapter.onTurnError = async (context, error) => {
-    // This check writes out errors to console log .vs. app insights.
+    // This check writes out errors to console log
+    // NOTE: In production environment, you should consider logging this to Azure
+    //       application insights.
     console.error(`\n [onTurnError]: ${ error }`);
     // Send a message to the user
     await context.sendActivity(`Oops. Something went wrong!`);
+    // Clear out state
+    await conversationState.clear(context);
+    // Save state changes.
+    await conversationState.saveChanges(context);
 };
 
-// Create the main dialog.
-const myBot = new MyBot();
+// Define a state store for your bot. See https://aka.ms/about-bot-state to learn more about using MemoryStorage.
+// A bot requires a state store to persist the dialog and user state between messages.
+let conversationState, userState;
 
-// Listen for incoming requests.
-server.post('/api/messages', (req, res) => {
-    adapter.processActivity(req, res, async (context) => {
-        // Route to main dialog.
-        await myBot.onTurn(context);
-    });
+let reference = 'reference';
+// CosmosDB storage
+// Create access to Cosmos DB storage.
+//Add CosmosDB 
+const { CosmosDbStorage } = require("botbuilder-azure");
+const memoryStorage = new CosmosDbStorage({
+    serviceEndpoint: process.env.ACTUAL_SERVICE_ENDPOINT, 
+    authKey: process.env.ACTUAL_AUTH_KEY, 
+    databaseId: process.env.DATABASE,
+    collectionId: process.env.COLLECTION
+})
+
+
+// For local development, in-memory storage is used.
+// CAUTION: The Memory Storage used here is for local bot debugging only. When the bot
+// is restarted, anything stored in memory will be gone.
+//const memoryStorage = new MemoryStorage();
+conversationState = new ConversationState(memoryStorage);
+userState = new UserState(memoryStorage);
+
+
+// console.log('userState-----userState')
+// console.log(userState)
+
+// CAUTION: You must ensure your product environment has the NODE_ENV set
+//          to use the Azure Blob storage or Azure Cosmos DB providers.
+
+// Add botbuilder-azure when using any Azure services. 
+// const { BlobStorage } = require('botbuilder-azure');
+// // Get service configuration
+// const blobStorageConfig = botConfig.findServiceByNameOrId(STORAGE_CONFIGURATION_ID);
+// const blobStorage = new BlobStorage({
+//     containerName: (blobStorageConfig.container || DEFAULT_BOT_CONTAINER),
+//     storageAccountOrConnectionString: blobStorageConfig.connectionString,
+// });
+// conversationState = new ConversationState(blobStorage);
+// userState = new UserState(blobStorage);
+
+// Create the main dialog.
+
+const LUIS_CONFIGURATION = 'BasicBotLuisApplication';
+
+const luisConfig = botConfig.findServiceByNameOrId(LUIS_CONFIGURATION);
+
+const luisApplication = {
+    applicationId: luisConfig.appId,
+    endpointKey: luisConfig.authoringKey,
+    endpoint: luisConfig.getEndpoint()
+};
+
+// Create configuration for LuisRecognizer's runtime behavior.
+const luisPredictionOptions = {
+    includeAllIntents: true,
+    log: true,
+    staging: false
+};
+
+let bot;
+try {
+    bot = new BasicBot(conversationState, userState, luisApplication, luisPredictionOptions);
+} catch (err) {
+    console.error(`[botInitializationError]: ${ err }`);
+    process.exit();
+}
+
+
+// Create HTTP server
+let server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function() {
+    console.log(`\n${ server.name } listening to ${ server.url }`);
+    console.log(`\nGet Bot Framework Emulator: https://aka.ms/botframework-emulator`);
+    console.log(`\nTo talk to your bot, open basic-bot.bot file in the Emulator`);
 });
+
+
+
+server.post('/api/messages', (req, res) => {
+    adapter.processActivity(req, res, async (turnContext) => {
+        await bot.onTurn(turnContext);
+        if (turnContext.activity.type === "message") {
+            reference = TurnContext.getConversationReference(turnContext.activity);
+        }
+    });
+    
+});
+
+server.use(restify.plugins.acceptParser(server.acceptable));
+server.use(restify.plugins.queryParser());
+server.use(restify.plugins.bodyParser());
+
+
+///   ffmpeg needs to call this endpoint to give the user the url his needs to open (demo through postman)
+server.post('/api/ffmpeg/test2', async (req, res, next) => {
+    try 
+    {
+        // res.send(req.body);
+        const obj = req.body;
+        const oref = obj.reference;
+ 
+        if (oref) {
+            // await adapter.continueConversation(oref, async (context) => {
+            //     const message = 'Use this link to open the video:' +  obj.url;
+            //     await context.sendActivity(message);
+            //     console.log(message);
+            // });
+
+            //try posting to Facebook API via Send API
+            let facebook_url = 'https://graph.facebook.com/v2.6/me/messages?access_token=EAAfMMp4oE0EBAJnHNtZCmWUDJv1JmXBD4g5NqdKLVW1b3GOScpybBKNImcONt0DeBEIdV5ZBTRVxN7U94EeYgSbjQQNXVK6MqY0GfqGw5vqL7tb04JZB59YlAyKeGVmZCaUnfPVYaZAeXLnlOiQZAI2WpCtxZCqRZA96kbDwc9tOvuMZAnHfB6zzD';
+            let post_obj = {
+                "recipient": { 
+                  "id": "2125277244228962"
+                },
+                "message":{
+                  "text":"Here is the url to your video: "+obj.url
+                },
+                "messaging_type": "MESSAGE_TAG",
+                "tag": "BUSINESS_PRODUCTIVITY"
+              };
+
+             axios.post(facebook_url, post_obj).then(function (response) {
+                console.log(response);
+                res.send({ message: "facebook message sent!"});
+              })
+              .catch(function (error) {
+                console.log(error);
+              });
+
+        };
+    } catch(err) {
+        console.log('error?');
+        res.send({message: err.message});
+        console.log(err);
+    }
+});
+
+    
+
+
+///  this endpoint posts the image to blob storage  (first step demo through facebook window)
+ server.get('/api/uploadimage', async (req, res) => {
+    console.log('here---index')
+    if (reference) {
+       await adapter.continueConversation(reference, async (context) => {
+         const text = JSON.stringify(reference);
+
+          const url = 'https://www.thelocal.fr/userdata/images/article/1e65a7b8481966489a47d4c3e2f8ac1a1ba04541731568ec8cb997f80fa0d246.jpg';
+        // console.log(url)
+        let containerName = 'ojoy-storage';
+        const response = await axios.get(url, { responseType: 'stream' });
+        // console.log(response);
+        let size = response.headers['content-length']
+        console.log(size)
+
+        //set conversation reference in blob metadata
+        var metav = { metadata: { "conversation_reference": JSON.stringify(reference) } };
+        
+        return new Promise( function(resolve, reject){
+            let filename_img = 'image_' + reference.user.id + '__' + Date.now()  +  '.jpg';
+            blobService.createBlockBlobFromStream(containerName, filename_img, response.data, size, metav, function(error){
+                if(!error){
+                    // Blob uploaded
+                    resolve('file uploaded');
+                } else {
+                    reject(error);
+                }
+            });
+
+        
+            // let filename_txt = 'reference_' + reference.user.id + '__' + Date.now() +  '.txt';
+            // blobService.createBlockBlobFromText(containerName, filename_txt, text,  function(error){
+
+            //     if(!error){
+            //         // Blob uploaded
+            //         resolve({ message: `Text "${text}" is written to blob storage` });
+            //     } else {
+            //         reject(error);
+            //     }
+            // });
+        })  
+       });
+       res.send(200);
+    } else {
+       res.send(404);
+    }
+ });
